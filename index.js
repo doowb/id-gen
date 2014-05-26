ee3791fb86cf0f1d2227fd9d73f81769749b95a1
Brian Woodward <brian.woodward@gmail.com>
2014-05-26 11:58:27 -0400

    initial migration and commit

---
+++ index.js
@@ -0,0 +1,108 @@
+
+var digits = require('digits');
+var _ = require('lodash');
+
+/**
+ * .defaultGenerator ( )
+ *
+ * Used in case a custom generator is not passed in.
+ *
+ * @return null
+ */
+
+function defaultGenerator () {
+  return null;
+}
+
+
+
+/**
+ * NameGenerator
+ *
+ * @constructor
+ * @param {Object} `options` groups of options to pass to `digits`
+ * @param {Function} `generator` custom generator used to calculate a name.
+ * @return {Object} new instance of a NameGenerator
+ *
+ */
+function NameGenerator (options, generator) {
+  if (!(this instanceof NameGenerator)) {
+    return new NameGenerator(options, generator);
+  }
+
+  if (_.isFunction(options)) {
+    generator = options;
+    options = {};
+  }
+
+  this.generator = generator || defaultGenerator;
+  this.options = options || {};
+  this.groups = {};
+  this.groups.default = {
+    options: this.options.default || {},
+    index: 0
+  };
+
+  _.forOwn(this.options, function (opts, key) {
+    // if the `digits` options are are the root
+    // just add them to default options
+    if (key === 'auto' || key === 'digits') {
+      this.groups.default.options[key] = opts;
+      return;
+    }
+    this.groups[key] = {
+      options: opts,
+      index: 0
+    };
+  }.bind(this));
+}
+
+
+/**
+ * .next (context)
+ *
+ * Generator the next name given the context and an optional group name.
+ * If the custom generator returns `null` or `undefined`, then the
+ * next name is calculated based on the group and `digits` options
+ * passed in the constructor.
+ *
+ * @param {Object} `context` passed directly to the custom generator
+ * @param {String} `group` name of the group to use when the custom generator doesn't return a name
+ * @return {String} The next name.
+ */
+NameGenerator.prototype.next = function (context, group) {
+  if (_.isString(context)) {
+    group = context;
+    context = {};
+  }
+  var name = this.generator(context);
+  if (_.isEmpty(name)) {
+    name = this._next(group);
+  }
+  return name;
+};
+
+
+/**
+ * ._next (group)
+ *
+ * Use digits to create the next name from the group.
+ * TODO: switch this to `strings` and allow a pattern instead of a prefix
+ *
+ * @param {String} `group` name of the group to use. defaults to 'default'
+ * @return {String} next generated name
+ *
+ */
+NameGenerator.prototype._next = function (name) {
+  name = name || 'default';
+  var group = this.groups[name];
+  group.index++;
+
+  var rtn = digits.pad(group.index, group.options);
+  rtn = (group.options.prefix || '') + rtn;
+  return rtn;
+};
+
+
+
+module.exports = NameGenerator;
---
+++ test/name-generator.js
@@ -0,0 +1,250 @@
+
+'use strict';
+
+var NameGenerator = require('../');
+var expect = require('chai').expect;
+
+describe('NameGenerator', function () {
+
+  describe('when initializing without any arguments', function () {
+
+    var nameoromic;
+
+    beforeEach(function () {
+      nameoromic = new NameGenerator();
+    });
+
+    it ('should return a default name', function () {
+      var expected = '001';
+      var actual = nameoromic.next();
+      expect(actual).to.be.eql(expected);
+    });
+
+    it ('should return default names in sequence', function () {
+      var i;
+      for (i = 1; i <= 10; i++) {
+        var expected = (i===10) ? '010' : '00' + i;
+        var actual = nameoromic.next();
+        expect(actual).to.be.eql(expected);
+      }
+    });
+
+
+    it ('should return proper names greater than 1000', function () {
+      var i;
+      for (i = 1; i <= 1010; i++) {
+        var actual = nameoromic.next();
+        if (i >= 1000) {
+          var expected = String(i);
+          expect(actual).to.be.eql(expected);
+        }
+      }
+    });
+
+  });
+
+  describe('when initializing with default options and no generator', function () {
+
+    var nameoromic;
+
+    beforeEach(function () {
+      var options = {
+        digits: 5
+      };
+      nameoromic = new NameGenerator(options);
+    });
+
+    it ('should return a default name', function () {
+      var expected = '00001';
+      var actual = nameoromic.next();
+      expect(actual).to.be.eql(expected);
+    });
+
+    it ('should return default names in sequence', function () {
+      var i;
+      for (i = 1; i <= 10; i++) {
+        var expected = (i===10) ? '00010' : '0000' + i;
+        var actual = nameoromic.next();
+        expect(actual).to.be.eql(expected);
+      }
+    });
+
+
+    it ('should return proper names greater than 100000', function () {
+      var i;
+      for (i = 1; i <= 100010; i++) {
+        var actual = nameoromic.next();
+        if (i >= 100000) {
+          var expected = String(i);
+          expect(actual).to.be.eql(expected);
+        }
+      }
+    });
+
+  });
+
+
+  describe('when initializing with grouped options and no generator', function () {
+
+    var nameoromic;
+
+    beforeEach(function () {
+      var options = {
+        'page': {
+          digits: 5,
+          prefix: 'Page-'
+        }
+      };
+      nameoromic = new NameGenerator(options);
+    });
+
+    it ('should return a default name', function () {
+      var expected = '001';
+      var actual = nameoromic.next();
+      expect(actual).to.be.eql(expected);
+    });
+
+    it ('should return default names in sequence', function () {
+      var i;
+      for (i = 1; i <= 10; i++) {
+        var expected = (i===10) ? '010' : '00' + i;
+        var actual = nameoromic.next();
+        expect(actual).to.be.eql(expected);
+      }
+    });
+
+    it ('should return a page name', function () {
+      var expected = 'Page-00001';
+      var actual = nameoromic.next('page');
+      expect(actual).to.be.eql(expected);
+    });
+
+    it ('should return page names in sequence', function () {
+      var i;
+      for (i = 1; i <= 10; i++) {
+        var expected = 'Page-' + ((i===10) ? '00010' : '0000' + i);
+        var actual = nameoromic.next('page');
+        expect(actual).to.be.eql(expected);
+      }
+    });
+
+    it ('should keep the correct index for each group', function () {
+      expect(nameoromic.next()).to.be.eql('001');
+      expect(nameoromic.next('page')).to.be.eql('Page-00001');
+      expect(nameoromic.next()).to.be.eql('002');
+      expect(nameoromic.next()).to.be.eql('003');
+      expect(nameoromic.next()).to.be.eql('004');
+      expect(nameoromic.next('page')).to.be.eql('Page-00002');
+      expect(nameoromic.next('page')).to.be.eql('Page-00003');
+      expect(nameoromic.next()).to.be.eql('005');
+      expect(nameoromic.next()).to.be.eql('006');
+      expect(nameoromic.next()).to.be.eql('007');
+      expect(nameoromic.next('page')).to.be.eql('Page-00004');
+    });
+
+  });
+
+
+  describe('when initializing a custom generator', function () {
+
+    var nameoromic;
+
+    beforeEach(function () {
+      var generator = function (context) {
+        context = context || {};
+        return context['_id'] || context['name'];
+      };
+      nameoromic = new NameGenerator(generator);
+    });
+
+    describe('when no context is passed in', function () {
+      it ('should return a default name', function () {
+        var expected = '001';
+        var actual = nameoromic.next();
+        expect(actual).to.be.eql(expected);
+      });
+    });
+
+    describe('when a context with an `_id` field is passed in', function () {
+      it ('should return the name as the `_id` field', function () {
+        var expected = 'my-id';
+        var actual = nameoromic.next({'_id': 'my-id'});
+        expect(actual).to.be.eql(expected);
+      });
+    });
+
+    describe('when a context with an `name` field is passed in', function () {
+      it ('should return the name as the `name` field', function () {
+        var expected = 'my-name';
+        var actual = nameoromic.next({'name': 'my-name'});
+        expect(actual).to.be.eql(expected);
+      });
+    });
+
+    describe('when a context without the proper fields is passed in', function () {
+      it ('should return a default name', function () {
+        var expected = '001';
+        var actual = nameoromic.next({foo: 'bar', baz: 'bang'});
+        expect(actual).to.be.eql(expected);
+      });
+    });
+
+  });
+
+
+  describe('when initializing a custom generator and options', function () {
+
+    var nameoromic;
+
+    beforeEach(function () {
+      var options = {
+        default: {
+          digits: 5,
+          prefix: 'custom-'
+        }
+      };
+      var generator = function (context) {
+        context = context || {};
+        return context['_id'] || context['name'];
+      };
+      nameoromic = new NameGenerator(options, generator);
+    });
+
+    describe('when no context is passed in', function () {
+      it ('should return a default name', function () {
+        var expected = 'custom-00001';
+        var actual = nameoromic.next();
+        expect(actual).to.be.eql(expected);
+      });
+    });
+
+    describe('when a context with an `_id` field is passed in', function () {
+      it ('should return the name as the `_id` field', function () {
+        var expected = 'my-id';
+        var actual = nameoromic.next({'_id': 'my-id'});
+        expect(actual).to.be.eql(expected);
+      });
+    });
+
+    describe('when a context with an `name` field is passed in', function () {
+      it ('should return the name as the `name` field', function () {
+        var expected = 'my-name';
+        var actual = nameoromic.next({'name': 'my-name'});
+        expect(actual).to.be.eql(expected);
+      });
+    });
+
+    describe('when a context without the proper fields is passed in', function () {
+      it ('should return a default name', function () {
+        var expected = 'custom-00001';
+        var actual = nameoromic.next({foo: 'bar', baz: 'bang'});
+        expect(actual).to.be.eql(expected);
+      });
+    });
+
+  });
+
+
+
+});
+
